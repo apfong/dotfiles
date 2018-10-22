edc42507007f5fae3132e0a4d4dff4b0e20adcf1
Alexander Fong <alpfong@gmail.com>
2018-10-22 14:30:49 -0700

    Organized files and added instructions to add tpm for tmux

---
+++ tmux/README.md
@@ -0,0 +1,15 @@
+# Setup
+---
+
+1. Run tmux
+
+```
+tmux
+```
+
+2. In tmux, install plugins
+
+```
+CTRL + b -> I (capital i)
+```
+
--- viminstall.sh
+++ viminstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Installation script for vim dotfiles
-mv .vim ~
-mv .vimrc ~
+mv vim/.vim ~
+mv vim/.vimrc ~
+mv tmux/.tmux.conf ~
+mv git/.gitconfig ~
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sleep 1
